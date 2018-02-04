8972f23c155d33169828fea83ce7f359e8a8c6ed
shababali <shabab.ali@mail.utoronto.ca>
2018-02-03 19:01:53 -0800

    PeecheeBeautyBar webpage

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  //sticky header
+  //$("#navbar").stick_in_parent();
+
+  //scroll it (use the data-scroll-nav to link up items to scrolling nav)
+  $.scrollIt();
+
+
+});
